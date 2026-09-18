05d630426a9bb393af1d7fb5c7e60900ae4fc6de
Felicity <felicity@hermes.local>
2026-09-18 20:03:09 +0530

    chore: bump the shell cache v9 -> v10 for the video-export fix

--- static/sw.js
+++ static/sw.js
@@ -19,7 +19,7 @@
  * controllerchange handler in templates/index.html.
  */
 
-const VERSION = 'v9';
+const VERSION = 'v10';
 const SHELL_CACHE = 'xygg-shell-' + VERSION;
 const API_CACHE = 'xygg-api-' + VERSION;
 const FONT_CACHE = 'xygg-fonts-' + VERSION;
